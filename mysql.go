6065c9e64ecc96d046417e6e14da051c2add95a3
pefish <darrondarron@qq.com>
2023-12-18 17:49:19 +0800

    v0.10.5

--- mysql.go
+++ mysql.go
@@ -1037,7 +1037,7 @@ func (mysql *builderClass) structToMap(in_ interface{}, result map[string]interf
 		jsonTag := fieldType.Tag.Get("json")
 		if jsonTag != "" {
 			jsonTags := strings.Split(jsonTag, ",")
-			if go_format.FormatInstance.IsZeroValue(field) {
+			if jsonTags[1] == "omitempty" && go_format.FormatInstance.IsZeroValue(field) { // 如果标记了omitempty且是零值，则不映射到 map 中
 				continue
 			}
 			key = jsonTags[0]
